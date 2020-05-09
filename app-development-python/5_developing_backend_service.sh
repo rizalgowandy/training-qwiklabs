246716f63e2f7d27ca9ea27d32c6f65a3a4a8505
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2020-05-09 22:17:33 +0700

    Python Developing a Backend Service

--- app-development-python/5_developing_backend_service.sh
+++ app-development-python/5_developing_backend_service.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+cd ~/training-data-analyst/courses/developingapps/python/pubsub-languageapi-spanner/start || exit 1
+. prepare_web_environment.sh
+python run_server.py
+
+# second terminal
+cd ~/training-data-analyst/courses/developingapps/python/pubsub-languageapi-spanner/start || exit 1
+. run_worker.sh
+
+# create pubsub topic feedback
+gcloud pubsub subscriptions create worker-subscription --topic feedback
+gcloud pubsub topics publish feedback --message "Hello World"
+gcloud beta pubsub subscriptions pull worker-subscription --auto-ack
+
+# api.py
+# pubsub.py
+# worker.py
+# languageapi.py
+# spanner.py
