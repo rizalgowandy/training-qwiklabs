621bd307cdf35ddce03d8e41e5485b0cb30766ec
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2021-03-17 17:11:27 +0700

    Add Log

--- serverless-cloud-run-development/challenge_lab.sh
+++ serverless-cloud-run-development/challenge_lab.sh
@@ -14,6 +14,8 @@ gcloud run deploy public-billing-service \
   --platform managed \
   --region us-central1 \
 
+echo ">>>> DONE: Deploy a Public Billing Service"
+
 cd ~/pet-theory/lab07/staging-frontend-billing || exit 1
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1
@@ -23,6 +25,8 @@ gcloud run deploy frontend-staging-service \
   --allow-unauthenticated \
   --platform managed \
   --region us-central1 \
+
+echo ">>>> DONE: Deploy the Frontend Service"
 
 cd ~/pet-theory/lab07/staging-api-billing || exit 1
 gcloud beta run services delete public-billing-service
@@ -36,12 +40,16 @@ gcloud run deploy private-billing-service \
   --platform managed \
   --region us-central1 \
 
+echo ">>>> DONE: Deploy a Private Billing Service"
+
 BILLING_SERVICE=private-billing-service
 BILLING_URL=$(gcloud run services describe $BILLING_SERVICE --format "value(status.URL)")
 
 curl -X get -H "Authorization: Bearer $(gcloud auth print-identity-token)" $BILLING_URL
 
 gcloud iam service-accounts create billing-service-sa --display-name "Billing Service Cloud Run"
+
+echo ">>>> DONE: Create a Billing Service Account"
 
 cd ~/pet-theory/lab07/prod-api-billing || exit 1
 gcloud builds submit \
@@ -69,6 +77,8 @@ curl -X get -H "Authorization: Bearer \
 
 gcloud iam service-accounts create frontend-service-sa --display-name "Billing Service Cloud Run Invoker"
 
+echo ">>>> DONE: Deploy a Billing Service in Production"
+
 cd ~/pet-theory/lab07/prod-frontend-billing || exit 1
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-prod:0.1
@@ -79,6 +89,10 @@ gcloud run deploy frontend-prod-service \
   --platform managed \
   --region us-central1 \
 
+echo ">>>> DONE: Create a Frontend Service Account"
+
 gcloud run services add-iam-policy-binding frontend-prod-service \
   --member=serviceAccount:frontend-service-sa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
   --role=roles/run.invoker
+
+echo ">>>> DONE: Deploy the Frontend Service in Production"
